243a58f9547e7fc013183755507801d8030a1141
alisaid-sm <alisaid7170@gmail.com>
2024-02-12 10:23:07 +0700

    all basic code java

---
+++ AccessModifier/src/com/dicoding/javafundamental/accessmodifier/package4/Lingkaran.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.accessmodifier.package4;
+
+final class Lingkaran {
+    static final double PI = 3.141;
+
+    int jari = 7;
+
+    final double getLuas() {
+        return PI * (jari * jari);
+    }
+}
